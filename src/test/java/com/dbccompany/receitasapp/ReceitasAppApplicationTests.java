59d0fb8c00140e7fd94ac9129cbd6a22f289c250
ezequiasf <ezequiasbarros1997@gmail.com>
2022-03-24 19:33:57 -0300

    inicio do projeto

---
+++ src/test/java/com/dbccompany/receitasapp/ReceitasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbccompany.receitasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
